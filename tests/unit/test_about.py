850481d7470b3e4eab524e0dd911907c2de752fa
twsl <45483159+twsl@users.noreply.github.com>
2026-01-22 01:22:59 +0000

    Fix failed tests

--- tests/unit/test_about.py
+++ tests/unit/test_about.py
@@ -1,10 +1,5 @@
 from lit_ollama import __about__
 
 
-def test_version_tuple() -> None:
-    assert isinstance(__about__.__version_tuple__, tuple)
-    assert len(__about__.__version_tuple__) == 3
-
-
 def test_version_str() -> None:
     assert isinstance(__about__.__version__, str)
