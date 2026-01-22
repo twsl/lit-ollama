0955186eae0b9286a0a3623647a90a72dbb70798
twsl <45483159+twsl@users.noreply.github.com>
2026-01-22 01:26:37 +0000

    Fix another import

--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -1,5 +1,4 @@
 def test_import_version():
-    from lit_ollama import __version__, __version_tuple__
+    from lit_ollama import __version__
 
     assert isinstance(__version__, str)
-    assert isinstance(__version_tuple__, tuple)
